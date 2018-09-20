2f8eec02230424747ed8109fcd9129af3ec5ea98
Evelyn Perez <evelyn@eokoe.com>
2018-09-20 19:15:48 -0300

    Add color on facebook chat icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,8 @@
     attribution=setup_tool
     page_id="398845206932703"
     logged_in_greeting="Faltam poucos dias pro primeiro turno, mas ainda precisamos levar o nosso projeto de combate aos privilégios, machismo, racismo e homofobia para muitas pessoas. Doe e vamos juntos!"
-    logged_out_greeting="Faltam poucos dias pro primeiro turno, mas ainda precisamos levar o nosso projeto de combate aos privilégios, machismo, racismo e homofobia para muitas pessoas. Doe e vamos juntos!">
+    logged_out_greeting="Faltam poucos dias pro primeiro turno, mas ainda precisamos levar o nosso projeto de combate aos privilégios, machismo, racismo e homofobia para muitas pessoas. Doe e vamos juntos!"
+    theme_color="#ffcd04">
   </div>
   <article class="home__intro">
     <div class="container">
