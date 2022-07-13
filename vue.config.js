3e8041618ba69a023e0579bbc11411f979f7a59d
sobral <sobral@appcivico.com>
2022-07-13 17:34:21 -0300

    Prepend sass abstract data to all styles

--- vue.config.js
+++ vue.config.js
@@ -11,4 +11,29 @@ module.exports = {
       }),
     ],
   },
+  css: {
+    loaderOptions: {
+      sass: {
+        webpackImporter: true,
+        additionalData: `
+        // 2. Vendors
+        @import '@/stylesheets/vendor/modularscale';
+
+        // 1. Configuration and helpers
+        @import
+        '@/stylesheets/abstracts/constants',
+        '@/stylesheets/abstracts/font-stacks',
+        '@/stylesheets/abstracts/functions',
+        '@/stylesheets/abstracts/variables',
+        '@/stylesheets/abstracts/mixins';
+
+        // 3. Base stuff
+        @import
+        '@/stylesheets/base/base',
+        '@/stylesheets/base/fonts',
+        '@/stylesheets/base/typography';
+        `
+      }
+    }
+  }
 };
