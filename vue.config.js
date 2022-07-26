3ae7255547f0601e2371d0018fe75750cec8a40f
sobral <sobral@appcivico.com>
2022-07-26 14:34:48 -0300

    Obfuscate code on build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+const WebpackObfuscator = require('webpack-obfuscator');
+
 module.exports = {
   lintOnSave: true,
+
+  configureWebpack: {
+    plugins: [
+      new WebpackObfuscator({
+        rotateStringArray: true,
+        identifierNamesGenerator: 'mangled',
+      }),
+    ],
+  },
 };
