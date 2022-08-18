94376e3c73d86f593e5160c93bb8e34ba2b55cc4
Sobral <servers@eokoe.com>
2022-08-17 23:49:17 -0300

    Improve selection of values

--- src/components/steps/selectValue.vue
+++ src/components/steps/selectValue.vue
@@ -6,41 +6,59 @@
     <div class="donation-intro" v-if="candidate.donation_intro" v-html="parseMD(candidate.donation_intro)"></div>
 
     <form @submit.prevent="validateForm()">
-      <fieldset class="of-radios-and-checks">
-        <div class="input-wrapper" v-for="pledge in pledges" :key="pledge.value"
-          :class="{ 'input-wrapper--has-label': !!pledge.label}">
-          <input type="radio" :id="`amount_${pledge.value}`" name="amount" v-model="amount" :value="pledge.value"
-            @change="validateForm()">
-          <label :for="`amount_${pledge.value}`" class="bigger">
-            <strong v-if="pledge.label">{{ pledge.label }}</strong>
-            <small v-if="!centsInUse">R$</small>
-            {{ pledge.value | formatBRL }}<small v-if="centsInUse">,{{ pad(pledge.value % 100, 2, '0') }}</small>
-          </label>
-        </div>
-        <transition name="custom-value-fade" mode="out-in" v-if="!candidate.disable_custom_donation_value">
-          <div class="input-wrapper
-          input-wrapper--full-width" v-if="amount !== 'other'" key="other">
-            <input type="radio" id="amount_other" name="amount" v-model="amount" value="other">
-            <label for="amount_other">Outro valor</label>
+      <fieldset v-for="fieldSet, i in pledgesAsFieldSets" class="of-radios-and-checks" :key="i">
+        <legend class="of-radios-and-checks__legend" v-if="fieldSet.legend">{{ fieldSet.legend }}</legend>
+
+        <div class="of-radios-and-checks__intro" v-if="fieldSet.intro" v-html="parseMD(fieldSet.intro)" />
+
+        <template v-for="pledge in fieldSet.fields">
+          <div class="input-wrapper" v-if="pledge.value !== 'other'" :key="pledge.value"
+            :class="{ 'input-wrapper--has-label': !!pledge.label}">
+            <input type="radio" :id="`amount_${pledge.value}`" name="amount" v-model="amount" :value="pledge.value"
+              @change="setButtonReferral(pledge.referral); validateForm();" />
+            <label :for="`amount_${pledge.value}`" class="bigger">
+              <strong v-if="pledge.label">{{ pledge.label }}</strong>
+              <small v-if="!centsInUse">R$</small>
+              {{ pledge.value | formatBRL }}<small v-if="centsInUse">,{{ pad(pledge.value % 100, 2, '0') }}</small>
+            </label>
           </div>
 
-          <div class="input-wrapper has-real-value" v-else key="customValue">
-            <label for="other">R$</label>
-            <input type="tel" name="other" v-model.number="other" @change="validateForm()" pattern="[0-9]*"
-              :disabled="amount === 'other' ? false : true" v-mask="'####'" v-focus>
-            <button type="button" href="#" @click.prevent="validateForm">OK</button>
-          </div>
-        </transition>
+          <transition name="custom-value-fade" mode="out-in" :key="pledge.value+'--other-amount-transition'"
+            v-else-if="!candidate.disable_custom_donation_value">
+            <div class="input-wrapper input-wrapper--full-width" :key="`${pledge.value}--custom-amount`"
+              v-if="amount !== 'other' || focusedFieldSet !== i">
+              <input type="radio" id="amount_other" name="amount" v-model="amount" value="other" />
+              <label for="amount_other" @click="setInitialValueForOther(fieldSet.minValue);
+              setButtonReferral(pledge.referral); focusedFieldSet = i">
+                <template v-if="pledge.label">
+                  {{ pledge.label }}
+                </template>
+                <template v-else-if="fieldSet.minValue">
+                  <small v-if="!centsInUse">R$</small>{{ fieldSet.minValue | formatBRL }} ou +
+                </template>
+                <template v-else>Outro valor</template>
+              </label>
+            </div>
+            <div class="input-wrapper has-real-value" :key="pledge.value+'--other-amount'"
+              v-else-if="focusedFieldSet === i">
+              <label for="other">R$</label>
+              <input type="tel" name="other" v-model.number="other" @change="validateForm()" pattern="[0-9]*"
+                :disabled="amount === 'other' ? false : true" v-mask="'####'" v-focus.select />
+              <button type="button" href="#" @click.prevent="validateForm">OK</button>
+            </div>
+          </transition>
+        </template>
+        <p class="error" v-if="errorMessage != '' && focusedFieldSet === i">
+          {{ errorMessage }}
+        </p>
       </fieldset>
-      <p class="error" v-if="errorMessage != ''">
-        {{ errorMessage }}
-      </p>
     </form>
   </section>
 </template>
 
 <script>
 import { mask } from 'vue-the-mask';
+import { mapMutations, mapState } from 'vuex';
 import {
 formatBRL, formatBRLDec, getQueryString, pad, parseMD, validate
 } from '../../utilities';
@@ -56,10 +74,19 @@ export default {
       amount: '',
       other: '',
       valueCheckedOnUrl: false,
+      buttonReferral: '',
+      focusedFieldSet: 0,
+      minValueError: '',
     };
   },
   mounted() {
     this.getDonationAmount();
+
+    if (this.referral.indexOf(':') > -1) {
+      const cleanReferral = this.referral.replace(`${this.buttonReferral}:`, '');
+      this.SET_REFERRAL(cleanReferral);
+    }
+    this.buttonReferral = '';
   },
   computed: {
     candidate() {
@@ -68,16 +95,29 @@ export default {
     centsInUse({ pledges } = this) {
       return pledges.some((x) => x % 100 !== 0);
     },
-    pledges({ candidate } = this) {
+    pledgesAsFieldSets({ candidate, cleanUpPledges } = this) {
       const {
-        pledges = [2000, 5000, 11000, 20000, 25000, 32000, 50000, 75000, 106400],
+        pledges = [2000, 5000, 11000, 20000, 25000, 32000, 50000, 75000, 106400, 'other'],
       } = candidate;
 
-      return pledges
-        .map((x) => (typeof x === 'number' ? { value: x } : x))
-        .filter((x) => !!x.value)
-        .sort((a, b) => b.value - a.value);
+      return candidate.pledgesAsFieldSets
+        .map((x) => ({
+          ...x,
+          fields: x.minValue ? cleanUpPledges(x.fields, x.minValue) : cleanUpPledges(x.fields),
+        }))
+        || {
+          fields: pledges,
+        };
     },
+
+    pledges({ pledgesAsFieldSets } = this) {
+      return pledgesAsFieldSets
+        .reduce((acc, cur) => ([...acc, ...cur.fields]), [])
+        .filter((x) => typeof x.value === 'number')
+        .map((x) => x.value);
+    },
+
+    ...mapState(['referral']),
   },
   watch: {
     candidate(newValue) {
@@ -87,24 +127,65 @@ export default {
     },
   },
   methods: {
+    cleanUpPledges(pledges, minValue = 0) {
+      return pledges.map((x) => (typeof x === 'number' || x === 'other' ? { value: x } : x))
+        .filter((x) => !!x.value || (x.value !== 'other' && typeof x.value !== 'number'))
+        .filter((x) => x.value === 'other' || x.value >= (minValue || 0))
+        .filter((obj, index, arr) => arr.map((mapObj) => mapObj.value).indexOf(obj.value) === index)
+        .sort((a, b) => {
+          if (typeof a.value === 'number' && typeof b.value === 'number') {
+            return b.value - a.value;
+          }
+          // check for num vs string
+          if (typeof a.value === 'number' && typeof b.value === 'string') {
+            return -1;
+          }
+          // check for string vs num
+          if (typeof a.value === 'string' && typeof b.value === 'number') {
+            return 1;
+          }
+          // check for string vs string
+          if (typeof a.value === 'string' && typeof b.value === 'string') {
+            if (a.value < b.value) return -1;
+            return 1;
+          }
+          return 0;
+        });
+    },
+    setButtonReferral(referral) {
+      this.buttonReferral = referral || '';
+    },
+    setInitialValueForOther(value) {
+      if (value) {
+        this.other = value / 100;
+      }
+    },
     validateForm() {
-      const { amount, other } = this;
+      const {
+        amount, buttonReferral, focusedFieldSet, other, pledgesAsFieldSets, referral,
+      } = this;
       const values = amount === 'other' ? { amount, other: other * 100 } : { amount };
-      const maxValue = this.candidate ? this.candidate.max_donation_value : 106409;
-      const minValue = this.candidate ? this.candidate.min_donation_value : 1000;
+      const maxValue = this.candidate?.max_donation_value || 106409;
+      const minValue = pledgesAsFieldSets[focusedFieldSet]?.minValue || this.candidate?.min_donation_value || 1000;
 
+      console.debug('minValue', minValue);
       const validation = validate(values);
 
       if (amount === 'other' && values.other < minValue) {
-        this.errorMessage = `O valor mínimo da doação é de R$ ${formatBRLDec(minValue)}`;
+        this.errorMessage = pledgesAsFieldSets[focusedFieldSet]?.minValue
+          ? `O valor mínimo para essa opção é de R$ ${formatBRLDec(minValue)}`
+          : `O valor mínimo para doação é de R$ ${formatBRLDec(minValue)}`;
         return;
       } if (amount === 'other' && values.other > maxValue) {
-        this.errorMessage = `O valor máximo da doação é de R$ ${formatBRLDec(maxValue)}`;
+        this.errorMessage = `O valor máximo para doação é de R$ ${formatBRLDec(maxValue)}`;
         return;
       }
 
       if (validation.valid) {
         this.saveStep(values);
+        if (buttonReferral) {
+          this.SET_REFERRAL(`${buttonReferral}:${referral}`);
+        }
       } else {
         this.errorMessage = 'Todos os campos são obrigatórios';
       }
@@ -163,6 +244,7 @@ export default {
     formatBRL,
     pad,
     parseMD,
+    ...mapMutations(['SET_REFERRAL']),
   },
 };
 </script>
